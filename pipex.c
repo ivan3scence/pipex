7a0eac22bd0f697d2606f30ad7343d334c5b9c9c
ivan3scence <ivan.gorich@gmail.com>
2022-01-11 00:09:19 +0300

    a

--- pipex.c
+++ pipex.c
@@ -1,43 +1,29 @@
-#include "./includes/pipex.h"
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdonny <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/10 16:06:53 by sdonny            #+#    #+#             */
+/*   Updated: 2022/01/10 16:06:58 by sdonny           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-//t_pip	*create_pip(void)
-//{
-//	t_pip	*elem;
-//
-//	elem = (t_pip *)malloc(sizeof(t_pip));
-//	if (!elem)
-//	{
-//		perror("not creating pip\n");
-//		exit(1);
-//	}
-//	elem->infile = NULL;
-//	elem->outfile = NULL;
-//	elem->comands = NULL;
-//	return (elem);
-//}
-//
-//void	del_elem(t_pip *pip)
-//{
-//	if (pip)
-//	{
-//		free(pip);
-//		pip = NULL;
-//	}
-//}
+#include "./includes/pipex.h"
+
+#include <stdio.h>
 
 static void	check_infile(char *infile)
 {
 	if (access(infile, F_OK) != 0)
 	{
 		perror(infile);
-		strerror(errno);
 		exit(1);
 	}
 	if (access(infile, R_OK) != 0)
 	{
 		perror(infile);
-		strerror(errno);
 		exit(1);
 	}
 }
@@ -49,108 +35,29 @@ static void	check_outfile(char *outfile)
 	if (access(outfile, W_OK) != 0)
 	{
 		perror(outfile);
-		strerror(errno);
 		exit(1);
 	}
 }
 
-//t_pip	*create_elem(int argc, char **argv)
-//{
-//	t_pip	*pip;
-//
-//	pip = create_pip();
-//	pip->infile = argv[1];
-//	pip->outfile = argv[argc - 1];
-//	check_infile(pip);
-//	check_outfile(pip);
-//	pip->comands = &argv[2];
-//	argv[argc] = NULL;
-//	return (pip);
-//}
-
-//t_pip	*validate(int argc, char **argv)
-//{
-//	t_pip	*pip;
-//
-//	if (argc < 3)
-//	{
-//		ft_putstr_fd("too few arguments", 2);
-//		exit(1);
-//	}
-//	pip = create_elem(argc, argv);
-//	return (pip);
-//}
-/*
-static void	change_stdout(char *outfile)
+static void	close_fd(int quantity, int proc, int **fd)
 {
-	int	fd;
+	int	i;
+	int	j;
 
-	unlink(outfile);
-	fd = open(outfile, O_WRONLY | O_CREAT, 0777);
-	if (fd == -1)
-			exit(1);
-	*{
-		perror(pip->outfile
-						);
-		del_elem(pip);
-		strerror(errno);
-		exit(1);
-	}
-	dup2(fd, STDOUT_FILENO);
-	close(1);
-}
-
-static void	change_stdoutt(int fd)
-{
-
-	dup2(fd, STDOUT_FILENO);
-	close(STDOUT_FILENO);
-}
-
-static void	change_stdin(char *infile)
-{
-	int	fd;
-
-	fd = open(infile, O_RDONLY);
-	if (fd == -1)
-			exit(1);
+	i = -1;
+	while (++i < quantity)
 	{
-		perror(infile);
-		//del_elem(pip);
-		strerror(errno);
-		exit(1);
-	}
-	dup2(fd, 0);
-	close(fd);
-}
-
-
-static void	change_stdinn(int fd)
-{
-	dup2(fd, 0);
-	close(0);
-}
-*/
-
-void    close_fd(int quantity, int proc, int **fd)
-{
-    int     i;
-    int     j;
-
-    i = -1;
-    while (++i < quantity)
-    {
-        j = -1;
-        while (++j < 2)
-        {
-            if (((i == proc && j == 0) || (i == proc + 1 && j == 1))
+		j = -1;
+		while (++j < 2)
+		{
+			if ((i == proc && j == 0) || (i == proc + 1 && j == 1)
 				|| (((i == quantity - 1 && j == 0) || (i == 0 && j == 1))
 					&& proc == -1))
-                continue ;
-            close(fd[i][j]);
-        }
-    }
-	if ( proc != -1)
+				continue ;
+			close(fd[i][j]);
+		}
+	}
+	if (proc != -1)
 	{
 		dup2(fd[proc][0], 0);
 		close(fd[proc][0]);
@@ -159,22 +66,24 @@ void    close_fd(int quantity, int proc, int **fd)
 	}
 }
 
-static void	validate(int argc, char **argv)
+static int	validate(int argc, char **argv)
 {
-	check_infile(argv[1]);
 	check_outfile(argv[argc - 1]);
+	if (ft_strncmp("here_doc", argv[1], 9) == 0)
+		return (1);
+	check_infile(argv[1]);
+	return (0);
 }
 
-static void	exitmalloc(int argc, int **fd)
+static void	exitmalloc(int quantity, int **fd)
 {
 	int	i;
 
 	i = -1;
-	perror("malloc for pipes\n");
-	strerror(errno);
+	perror("pipes\n");
 	if (fd)
 	{
-		while (i < argc - 2)
+		while (++i < quantity)
 			free(fd[i]);
 		free(fd);
 		fd = NULL;
@@ -182,38 +91,35 @@ static void	exitmalloc(int argc, int **fd)
 	exit(1);
 }
 
-static int	**multipipe(int argc)
+static int	**multipipe(int argc, int m)
 {
 	int	i;
-	int **fd;
+	int	**fd;
 
-	fd = (int **)malloc(sizeof(int *) * (argc - 2));
+	fd = (int **)malloc(sizeof(int *) * (argc - 2 - m));
 	if (!fd)
-		exitmalloc(argc, NULL);
-	i = 0;
-	while (i < argc - 2)
+		exitmalloc(0, fd);
+	i = -1;
+	ft_putnbr_fd(argc - 2 - m, 1);
+	while (++i < argc - 2 - m)
 	{
 		fd[i] = (int *)malloc(sizeof(int) * 2);
 		if (!fd[i])
-			exitmalloc(argc, NULL);
-		if (pipe(fd[i++]) == -1)
-		{
-			perror("pipe error");
-			strerror(errno);
-			exit(1);
-		}
+			exitmalloc(i, fd);
+		if (pipe(fd[i]) == -1)
+			exitmalloc(i, fd);
+		ft_putnbr_fd(fd[i][1], 1);
 	}
 	return (fd);
 }
 
-static void	exitpid(int **fd, pid_t *pid, int argc)
+static void	exitpid(int **fd, pid_t *pid, int quantity)
 {
 	int	i;
 
 	i = -1;
 	perror("pid - rip");
-	strerror(errno);
-	while (++i < argc - 2)
+	while (++i < quantity + 1)
 		free(fd[i]);
 	free(fd);
 	fd = NULL;
@@ -232,7 +138,6 @@ static char	**cmdparse(char *old)
 	new[0] = ft_strjoin("/bin/", tmp);
 	free(tmp);
 	tmp = NULL;
-	ft_putstr_fd(new[0], 1);
 	return (new);
 }
 
@@ -247,26 +152,24 @@ static void	cleancmd(char **cmd)
 	cmd = NULL;
 }
 
-static void	forks(int **fd, int argc, char	**argv, char **envp)
+static pid_t	*forks(int **fd, int quantity, char	**argv, char **envp)
 {
 	int		m;
-	int		p;
 	pid_t	*pid;
 	char	**cmd;
 
 	m = -1;
-	p = -1;
-	pid = (pid_t *)malloc(sizeof(pid_t) * (argc - 3));
+	pid = (pid_t *)malloc(sizeof(pid_t) * quantity);
 	if (!pid)
-		exitmalloc(argc, fd);
-	while (++m < argc - 3)
+		exitmalloc(quantity, fd);
+	while (++m < quantity)
 	{
-		pid[++p] = fork();
-		if (pid[p] < 0)
-			exitpid(fd, pid, argc);
-		if (!pid[p])
+		pid[m] = fork();
+		if (pid[m] < 0)
+			exitpid(fd, pid, quantity);
+		if (!pid[m])
 		{
-			close_fd(argc - 2, m, fd);
+			close_fd(quantity + 1, m, fd);
 			cmd = cmdparse(argv[m + 2]);
 			execve(cmd[0], cmd, envp);
 			cleancmd(cmd);
@@ -275,48 +178,99 @@ static void	forks(int **fd, int argc, char	**argv, char **envp)
 			exit(0);
 		}
 	}
+	return (pid);
 }
 
-int	main(int argc, char **argv, char **envp)
+static int	parentread(char *cmp, int fd, char *filename)
 {
-	int     **fd;
-	int     m;
-	int     pid[argc - 3];
-
-	validate(argc, argv);
-	m = -1;
-	fd = multipipe(argc);
-	forks(fd, argc, argv, envp);
-	
-	close_fd(argc - 2, -1, fd);
-	char *buf;
+	char	*buf;
 	int		file;
-	file = open("infile", O_RDONLY);
+
+	if (!cmp)
+		file = open(filename, O_RDONLY);
+	else
+		file = 0;
+	if (file == -1)
+		return (-1);
+	//ft_putstr_fd(cmp, 1);
 	while (1)
 	{
+		//ft_putstr_fd("gg\n", 1);
 		buf = get_next_line(file);
-		//ft_putstr_fd(buf, 1);
-		if (!buf)
+		if ((cmp && ft_strncmp(buf, cmp, ft_strlen(cmp)) == 0)
+			|| (!buf && !cmp))
 			break ;
-		ft_putstr_fd(buf, fd[0][1]);
+		ft_putstr_fd(buf, fd);
 		free(buf);
 	}
-	close(file);
-	file = open("outfile", O_WRONLY | O_CREAT, 0664);
-	close(fd[0][1]);
+	close(fd);
+	if (!cmp)
+		close(file);
+	return (0);
+}
+
+static int	parentwrite(int fd, char *filename, int flag)
+{
+	char	*buf;
+	int		file;
+
+	if (!flag)
+		file = open(filename, O_WRONLY | O_CREAT, 0664);
+	else
+		file = open(filename, O_APPEND | O_CREAT, 0664);
+	if (file == -1)
+		return (-1);
 	while (1)
 	{
-		buf = get_next_line(fd[argc - 3][0]);
+		buf = get_next_line(fd);
 		if (!buf)
 			break ;
 		ft_putstr_fd(buf, file);
 		free(buf);
 	}
+	close(fd);
 	close(file);
-	close(fd[argc - 3][0]);
+	return (0);
+}
+
+static void	waitchildren(pid_t *pid, int **fd, int argc)
+{
+	int	m;
+
 	m = -1;
 	while (++m < argc - 3)
+	{
 		waitpid(pid[m], NULL, 0);
+		free(fd[m]);
+	}
+	free(fd[m]);
 	free(fd);
-	exit(0);
+	free(pid);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	int	**fd;
+	int	m;
+	pid_t	*pid;
+
+	if (argc < 4)
+	{
+		ft_putstr_fd("Error!\nToo few arguments!\n", 2);
+		exit(1);
+	}
+	m = validate(argc, argv);
+	fd = multipipe(argc, m);
+	pid = forks(fd, argc - 3 - m, argv, envp);
+	close_fd(argc - 2 - m, -1, fd);
+	if (!m)
+		m = parentread(NULL, fd[0][1], argv[1]);
+	else
+		m = parentread(argv[2], fd[0][1], NULL);
+	if (m == -1)
+		exitpid(fd, pid, argc);
+	m = parentwrite(fd[argc - 3][0], argv[argc - 1], 1);
+	if (m == -1)
+		exitpid(fd, pid, argc);
+	waitchildren(pid, fd, argc);
 }
